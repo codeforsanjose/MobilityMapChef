47c18e9f7dbc0f008b9dee8a8b6406ab25cde5b4
Zeppelin <zpallin@gmail.com>
2015-01-22 20:13:09 -0800

    Chefdk fix

--- chefdk_path.sh
+++ chefdk_path.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo 'export PATH="/opt/chefdk/embedded/bin:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
+echo 'export PATH="/opt/chefdk/bin:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
